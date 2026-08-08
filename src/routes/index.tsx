97e5ea0a9f9bc06cb2c4c15139a01a12e63efbd0
emmasif789-pixel <emmasif789@gmail.com>
2026-08-08 13:08:37 +0500

    Add ExploreSearch component to main layout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Navbar } from "@/components/wistube/navbar";
 import { Hero } from "@/components/wistube/hero";
+import { ExploreSearch } from "@/components/wistube/explore-search";
 import { Features } from "@/components/wistube/features";
 import { Footer } from "@/components/wistube/footer";
 import { Aurora } from "@/components/wistube/aurora";
@@ -15,6 +16,7 @@ function Index() {
       <Aurora />
       <Navbar />
       <Hero />
+      <ExploreSearch />
       <Features />
       <Footer />
     </main>
