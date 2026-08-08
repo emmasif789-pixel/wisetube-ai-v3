029199944144c977ed1268e64acb07dece176c79
emmasif789-pixel <emmasif789@gmail.com>
2026-08-08 13:36:39 +0500

    Add searchVideos function for video search

--- src/lib/search.functions.ts
+++ src/lib/search.functions.ts
@@ -1,1 +1,9 @@
+import { createServerFn } from "@tanstack/react-start";
+import { z } from "zod";
+import { searchReports, type SearchHit } from "./algolia-client";
 
+export const searchVideos = createServerFn({ method: "GET" })
+  .inputValidator((input: unknown) => z.object({ query: z.string().min(1) }).parse(input))
+  .handler(async ({ data }): Promise<SearchHit[]> => {
+    return searchReports(data.query);
+  });
